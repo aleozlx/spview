453942bfb6cbb3d76d4f3ffe8a9013a42e3e8ce9
Alex Yang <aleozlx@gmail.com>
2020-01-26 15:27:30 -0600

    + dirscan as a go target

---
+++ cmake/GolangSimple.cmake
@@ -0,0 +1,30 @@
+# https://github.com/cpconduce/go_cmake
+# Note: it has been modified to support Windows
+set(GOPATH "${CMAKE_CURRENT_BINARY_DIR}/go")
+file(MAKE_DIRECTORY ${GOPATH})
+
+function(GO_GET TARG)
+    add_custom_target(${TARG} env GOPATH=${GOPATH} go get ${ARGN})
+endfunction(GO_GET)
+
+function(ADD_GO_INSTALLABLE_PROGRAM NAME MAIN_SRC)
+    get_filename_component(MAIN_SRC_ABS ${MAIN_SRC} ABSOLUTE)
+    if(WIN32)
+        set(BIN_NAME ${NAME}.exe)
+    else()
+        set(BIN_NAME ${NAME})
+    endif()
+    add_custom_target(${NAME}
+            ${CMAKE_COMMAND} -E env GOPATH=${GOPATH} go build
+            -o "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${BIN_NAME}"
+#            -o "${CMAKE_CURRENT_BINARY_DIR}/${BIN_NAME}"
+            ${CMAKE_GO_FLAGS} ${MAIN_SRC}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+            DEPENDS ${MAIN_SRC_ABS})
+#    foreach(DEP ${ARGN})
+#        add_dependencies(${NAME} ${DEP})
+#    endforeach()
+
+#    add_custom_target(${NAME}_all ALL DEPENDS ${NAME})
+#    install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${BIN_NAME} DESTINATION bin)
+endfunction(ADD_GO_INSTALLABLE_PROGRAM)
