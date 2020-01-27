5929264ab65a61269ba8ee568d49991fef7d8167
Alex Yang <aleozlx@gmail.com>
2020-01-27 11:17:19 -0600

    refactor

---
+++ AppDemo/MNIST.cpp
@@ -0,0 +1,28 @@
+#include "app.hpp"
+#include "teximage.hpp"
+
+#if _MSC_VER
+// Disables the console window on Windows
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
+
+using namespace spview;
+
+int main(int, char **) {
+    auto app = AppEngine::App::Initialize();
+    if (!app.ok) return 1;
+    ImGuiIO &io = ImGui::GetIO();
+    (void) io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+
+    while (app.EventLoop()) {
+        ImGui::Begin("MNIST");
+
+        ImGui::End();
+        app.Render(clear_color);
+    }
+
+    return 0;
+}
