214823f5277b3643960cc4291cd0b97ff627aec3
Alex Yang <aleozlx@gmail.com>
2020-01-27 13:45:45 -0600

    + imgui-plot That's something. It's a start.

--- AppDemo/MNIST.cpp
+++ AppDemo/MNIST.cpp
@@ -1,5 +1,6 @@
 #include "app.hpp"
 #include "teximage.hpp"
+#include "imgui_plot.h"
 
 #if _MSC_VER
 // Disables the console window on Windows
@@ -7,6 +8,26 @@
 #endif
 
 using namespace spview;
+
+const size_t buf_size = 512;
+static float x_data[buf_size];
+static float y_data1[buf_size];
+static float y_data2[buf_size];
+static float y_data3[buf_size];
+
+void generate_data() {
+    constexpr float sampling_freq = 44100;
+    constexpr float freq = 500;
+    for (size_t i = 0; i < buf_size; ++i) {
+        const float t = i / sampling_freq;
+        x_data[i] = t;
+        const float arg = 2 * 3.14159 * freq * t;
+        y_data1[i] = sin(arg);
+        y_data2[i] = y_data1[i] * -0.6 + sin(2 * arg) * 0.4;
+        y_data3[i] = y_data2[i] * -0.6 + sin(3 * arg) * 0.4;
+    }
+}
+
 
 int main(int, char **) {
     auto app = AppEngine::App::Initialize();
@@ -16,10 +37,60 @@ int main(int, char **) {
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    static const float* y_data[] = { y_data1, y_data2, y_data3 };
+    static ImU32 colors[3] = { ImColor(0, 255, 0), ImColor(255, 0, 0), ImColor(0, 0, 255) };
+    static uint32_t selection_start = 0, selection_length = 0;
+    generate_data();
 
     while (app.EventLoop()) {
         ImGui::Begin("MNIST");
+        ImGui::Button("Run");
+        ImGui::SameLine();
+        ImGui::Button("Dataset...");
+        ImGui::SameLine();
+        ImGui::Text("<Dataset Location>");
 
+        ImGui::Dummy(ImVec2(0, 5));
+        ImGui::Text("Epoch");
+        ImGui::SameLine(70);
+        ImGui::ProgressBar(0.1);
+        ImGui::Text("Step");
+        ImGui::SameLine(70);
+        ImGui::ProgressBar(0.1);
+
+
+
+        ImGui::PlotConfig conf;
+        conf.values.xs = x_data;
+        conf.values.count = buf_size;
+        conf.values.ys_list = y_data; // use ys_list to draw several lines simultaneously
+        conf.values.ys_count = 3;
+        conf.values.colors = colors;
+        conf.scale.min = -1;
+        conf.scale.max = 1;
+        conf.tooltip.show = true;
+        conf.grid_x.show = true;
+        conf.grid_x.size = 128;
+        conf.grid_x.subticks = 4;
+        conf.grid_y.show = true;
+        conf.grid_y.size = 0.5f;
+        conf.grid_y.subticks = 5;
+        conf.selection.show = true;
+        conf.selection.start = &selection_start;
+        conf.selection.length = &selection_length;
+        conf.frame_size = ImVec2(buf_size, 200);
+        ImGui::Plot("plot1", conf);
+
+        // Draw second plot with the selection
+        // reset previous values
+        conf.values.ys_list = nullptr;
+        conf.selection.show = false;
+        // set new ones
+        conf.values.ys = y_data3;
+        conf.values.offset = selection_start;
+        conf.values.count = selection_length;
+        conf.line_thickness = 2.f;
+        ImGui::Plot("plot2", conf);
         ImGui::End();
         app.Render(clear_color);
     }
